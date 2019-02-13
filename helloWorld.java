c5f23678300e0df0ca266c4d2f294949205b5045
Langston Beckwith-Stanley <langston.beckwithstanley@pcc.edu>
2019-02-12 19:28:51 -0800

    added date to the  main method

--- helloWorld.java
+++ helloWorld.java
@@ -1,8 +1,11 @@
 package hellowWorld;
 
+import java.util.Date;
+
 public class helloWorld {
     public static void main(String[] args){
 
         System.out.println("hello World");
+        System.out.println( new Date());
     }
 }
